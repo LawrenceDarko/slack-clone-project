17aa18c65ddf4ad07f1628192d38903f2fdbd420
LawrenceDarko <darkolawrence@gmail.com>
2021-08-09 13:09:55 +0800

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyATDpzkiKm0nVHx3VzkmKoF7_UpojB5DRk",
+    authDomain: "slack-clone-proj-779f9.firebaseapp.com",
+    projectId: "slack-clone-proj-779f9",
+    storageBucket: "slack-clone-proj-779f9.appspot.com",
+    messagingSenderId: "521627971913",
+    appId: "1:521627971913:web:5f8891bb5cc46c502c2a98"
+  };
