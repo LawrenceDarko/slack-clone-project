d42551ff1b7e526b5a4d00d7813e20634a5094c4
LawrenceDarko <darkolawrence@gmail.com>
2021-11-01 10:29:22 +0800

    Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyATDpzkiKm0nVHx3VzkmKoF7_UpojB5DRk",
     authDomain: "slack-clone-proj-779f9.firebaseapp.com",
@@ -6,3 +8,12 @@ const firebaseConfig = {
     messagingSenderId: "521627971913",
     appId: "1:521627971913:web:5f8891bb5cc46c502c2a98"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  // For Google authentication
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+
+  export { auth, provider, db };
